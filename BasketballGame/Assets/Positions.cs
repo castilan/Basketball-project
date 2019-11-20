f069a09b822b20af2bc0d2f597f268b8ebe1e1b0
MKrent <michael@krent.com>
2019-11-20 06:15:32 -0800

    Changed style, added passing

---
+++ BasketballGame/Assets/Positions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Positions : MonoBehaviour
+{
+    Vector3 StartPos;
+
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    public void ChangePos(int pos)
+    {
+        switch(pos)
+        {
+            case 0:
+                transform.position = new Vector3(StartPos.x, StartPos.y, StartPos.z);
+                break;
+            case 1:
+                transform.position = new Vector3(StartPos.x + 4.7f, StartPos.y + 4.2f, StartPos.z);
+                break;
+            case 2:
+                transform.position = new Vector3(StartPos.x - 5.3f, StartPos.y + 3.6f, StartPos.z);
+                break;
+            case 3:
+                transform.position = new Vector3(StartPos.x - 2.5f, StartPos.y + 4.2f, StartPos.z);
+                break;
+        }
+        
+    }
+}
