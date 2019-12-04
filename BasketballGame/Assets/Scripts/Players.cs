693788a7b0a203ff08e9b760924b22af3c8b34f0
MKrent <michael@krent.com>
2019-12-04 01:37:29 -0800

    Player stats added

--- BasketballGame/Assets/Scripts/Players.cs
+++ BasketballGame/Assets/Scripts/Players.cs
@@ -2,33 +2,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Players : MonoBehaviour
 {
 
     public playerData center = new playerData();
-    public playerData left;
-    public playerData right;
-    public playerData post;
-    public playerData centerCPU;
-    public playerData leftCPU;
-    public playerData rightCPU;
-    public playerData postCPU;
+    public playerData left = new playerData();
+    public playerData right = new playerData();
+    public playerData post = new playerData();
+    public playerData centerCPU = new playerData();
+    public playerData leftCPU = new playerData();
+    public playerData rightCPU = new playerData();
+    public playerData postCPU = new playerData();
+
+    public Text COName;
+    public Text LOName;
+    public Text ROName;
+    public Text POName;
+    public Text CDName;
+    public Text LDName;
+    public Text RDName;
+    public Text PDName;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("GameHandler.Start");
+        //Debug.Log("GameHandler.Start");
         allPlayers all = new allPlayers();
 
         string json = File.ReadAllText("Assets/players.json");
-        Debug.Log(json);
         all = JsonUtility.FromJson<allPlayers>(json);
         center = all.players[0];
-        Debug.Log(center.name);
-        //left = all.players[2];
+        left = all.players[1];
+        right = all.players[2];
+        post = all.players[3];
+        centerCPU = all.players[4];
+        leftCPU = all.players[5];
+        rightCPU = all.players[6];
+        postCPU = all.players[7];
 
-        
+        COName.text = center.name;
+        LOName.text = left.name;
+        ROName.text = right.name;
+        POName.text = post.name;
+        CDName.text = centerCPU.name;
+        LDName.text = leftCPU.name;
+        RDName.text = rightCPU.name;
+        PDName.text = postCPU.name;
     }
 
     // Update is called once per frame
